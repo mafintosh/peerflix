b02a770dd8b677f1aa468e37deed5c9700a310f1
Bogdan Kovalev <bogdan.kovalev@teamdev.com>
2016-11-22 20:55:15 +0200

    fix indentations

--- app.js
+++ app.js
@@ -137,7 +137,9 @@ var ontorrent = function (torrent) {
             return {
               name: file.name + ' : ' + bytes(file.length),
               value: i
-            } })}]).then(function (answers) {
+            }
+          })
+        }]).then(function (answers) {
           argv.index = answers.file
           delete argv.list
           ontorrent(torrent)
